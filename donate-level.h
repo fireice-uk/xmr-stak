3192822c72cbcc749338b2cc669261910def09cb
psychocrypt <psychocrypt@users.noreply.github.com>
2017-06-23 21:21:31 +0200

    add psychocrypt to donate-level.h

--- donate-level.h
+++ donate-level.h
@@ -7,8 +7,11 @@
  * You miner will mine into your usual pool for 99 minutes, then switch to the developer's pool for 1.0 minute.
  * Switching is instant, and only happens after a successful connection, so you never loose any hashes.
  *
- * If you plan on changing this setting to 0.0 please consider making a one off donation to my wallet:
+ * If you plan on changing this setting to 0.0 please consider making a one off donation to our wallets:
+ * fireice-uk:
  * 4581HhZkQHgZrZjKeCfCJxZff9E3xCgHGF25zABZz7oR71TnbbgiS7sK9jveE6Dx6uMs2LwszDuvQJgRZQotdpHt1fTdDhk
+ * psychocrypt:
+ * 43NoJVEXo21hGZ6tDG6Z3g4qimiGdJPE6GRxAmiWwm26gwr62Lqo7zRiCJFSBmbkwTGNuuES9ES5TgaVHceuYc4Y75txCTU
  *
  */
 
