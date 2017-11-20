99151ed31dd7bfbb7510c077c02fec88c0215e14
Lev Serebryakov <blacklion+git@gmail.com>
2017-11-21 02:13:44 +0300

    Fix uninitialized memory accces
    Uninitialized memory access is UB in C/C++, fix it.

--- xmrstak/misc/executor.cpp
+++ xmrstak/misc/executor.cpp
@@ -459,6 +459,7 @@ void executor::on_miner_result(size_t pool_id, job_result& oResult)
 void disable_sigpipe()
 {
 	struct sigaction sa;
+	memset(&sa, 0, sizeof(sa));
 	sa.sa_handler = SIG_IGN;
 	sa.sa_flags = 0;
 	if (sigaction(SIGPIPE, &sa, 0) == -1)
