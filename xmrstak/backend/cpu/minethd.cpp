1d60d43fbc24133d13321401c550785075c219e2
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-01-01 19:20:24 -0500

    It compiles on OpenBSD/adJ 6.2

--- xmrstak/backend/cpu/minethd.cpp
+++ xmrstak/backend/cpu/minethd.cpp
@@ -84,7 +84,7 @@ bool minethd::thd_setaffinity(std::thread::native_handle_type h, uint64_t cpu_id
 	CPU_ZERO(&mn);
 	CPU_SET(cpu_id, &mn);
 	return pthread_setaffinity_np(h, sizeof(cpuset_t), &mn) == 0;
-#else
+#elif !defined(__OpenBSD__)
 	cpu_set_t mn;
 	CPU_ZERO(&mn);
 	CPU_SET(cpu_id, &mn);
