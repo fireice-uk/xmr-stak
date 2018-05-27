01d174cfc868bdb35a75e8e8976036144ae3dace
Sean <sean@seanv.com>
2018-05-26 20:30:27 -0400

    Update setup_vm2.sh

--- azure_script/setup_vm2.sh
+++ azure_script/setup_vm2.sh
@@ -2,7 +2,7 @@
 sudo apt-get -y update       
 sudo apt-get -y upgrade  
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
-git clone https://github.com/azurecloudminingscript/azure-cloud-mining-script
+git clone https://github.com/osnabrugge/azure-cloud-mining-script
 
 cd azure-cloud-mining-script
 azure_script/compile_and_config.sh
