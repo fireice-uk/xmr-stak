2932de6951dc94a4cff7eec45b70dcda770b9bc9
psychocrypt <psychocryptHPC@gmail.com>
2018-09-16 22:23:36 +0200

    assembler version for cryptonight_v8
    Add @SChernykh assembler version for ryzen and intel processors.
    
    Co-authored-by: SChernykh <sergey.v.chernykh@gmail.com>

---
+++ xmrstak/backend/cpu/crypto/asm/cryptonigh_v8_main_loop.asm
@@ -0,0 +1,18 @@
+_TEXT_CNV8_MAINLOOP SEGMENT PAGE READ EXECUTE
+PUBLIC cryptonigh_v8_mainloop_ivybridge_asm
+PUBLIC cryptonigh_v8_mainloop_ryzen_asm
+
+ALIGN 64
+cryptonigh_v8_mainloop_ivybridge_asm PROC
+	INCLUDE cryptonigh_v8_main_loop_ivybridge.inc
+	ret 0
+cryptonigh_v8_mainloop_ivybridge_asm ENDP
+
+ALIGN 64
+cryptonigh_v8_mainloop_ryzen_asm PROC
+	INCLUDE cryptonigh_v8_main_loop_ryzen.inc
+	ret 0
+cryptonigh_v8_mainloop_ryzen_asm ENDP
+
+_TEXT_CNV8_MAINLOOP ENDS
+END
