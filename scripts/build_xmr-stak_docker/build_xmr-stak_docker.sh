5233e2db67070e139a51bfea55e85af478c9fbac
gzf445 <34760275+gzf445@users.noreply.github.com>
2018-10-13 18:12:08 +0800

    Update build_xmr-stak_docker.sh

--- scripts/build_xmr-stak_docker/build_xmr-stak_docker.sh
+++ scripts/build_xmr-stak_docker/build_xmr-stak_docker.sh
@@ -15,57 +15,6 @@ wget -c https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda
 chmod a+x cuda_*_linux-run
 
 
-########################
-# Fedora 27
-########################
-# CUDA is not going to work on Fedora 27 beacuse it only supports these distributions: http://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html
-docker run --rm -it -v $PWD:/mnt fedora:27 /bin/bash -c "
-set -x ;
-dnf install -y -q cmake gcc-c++ hwloc-devel libmicrohttpd-devel libstdc++-static make openssl-devel;
-cd /mnt/xmr-stak ;
-cmake -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF . ;
-make ;
-"
-
-test -d fedora_27 || mkdir fedora_27
-mv xmr-stak/bin/* fedora_27
-git -C xmr-stak clean -fd
-
-
-########################
-# Ubuntu (17.04)
-########################
-docker run --rm -it -v $PWD:/mnt ubuntu:17.04 /bin/bash -c "
-set -x ;
-apt update -qq ;
-apt install -y -qq libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev ;
-cd /mnt/xmr-stak ;
-/mnt/cuda_*_linux-run --silent --toolkit ;
-cmake -DCUDA_ENABLE=ON -DOpenCL_ENABLE=OFF . ;
-make ;
-"
-
-test -d ubuntu_17.10 || mkdir ubuntu_17.10
-mv xmr-stak/bin/* ubuntu_17.10
-git -C xmr-stak clean -fd
-
-
-########################
-# Ubuntu 16.04
-########################
-docker run --rm -it -v $PWD:/mnt ubuntu:16.04 /bin/bash -c "
-set -x ;
-apt update -qq ;
-apt install -y -qq cmake g++ libmicrohttpd-dev libssl-dev libhwloc-dev ;
-cd /mnt/xmr-stak ;
-/mnt/cuda_*_linux-run --silent --toolkit ;
-cmake -DCUDA_ENABLE=ON -DOpenCL_ENABLE=OFF . ;
-make ;
-"
-
-test -d ubuntu_16.04 || mkdir ubuntu_16.04
-mv xmr-stak/bin/* ubuntu_16.04
-git -C xmr-stak clean -fd
 
 
 ########################
@@ -93,43 +42,3 @@ mv xmr-stak/bin/* ubuntu_14.04
 git -C xmr-stak clean -fd
 
 
-########################
-# CentOS 7
-########################
-# CUDA is not going to work on CentOS/RHEL beacuse it's only support gcc-4 in these distributions: http://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html
-docker run --rm -it -v $PWD:/mnt centos:7 /bin/bash -c "
-set -x ;
-yum install -y -q centos-release-scl epel-release ;
-yum install -y -q cmake3 devtoolset-7-gcc* hwloc-devel libmicrohttpd-devel make openssl-devel perl ;
-scl enable devtoolset-7 - << EOF
-cd /mnt/xmr-stak ;
-cmake3 -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF . ;
-make ;
-EOF
-"
-
-test -d centos_7 || mkdir centos_7
-mv xmr-stak/bin/* centos_7
-git -C xmr-stak clean -fd
-
-
-########################
-# CentOS 6.x
-########################
-# CUDA is not going to work on CentOS/RHEL beacuse it's only support gcc-4 in these distributions: http://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html
-docker run --rm -it -v $PWD:/mnt centos:6 /bin/bash -c "
-set -x ;
-yum install -y -q centos-release-scl epel-release ;
-yum install -y -q cmake3 devtoolset-7-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make ;
-scl enable devtoolset-7 - << EOF
-cd /mnt/xmr-stak ;
-cmake3 -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF . ;
-make ;
-EOF
-"
-
-test -d centos_6 || mkdir centos_6
-mv xmr-stak/bin/* centos_6
-git -C xmr-stak clean -fd
-
-rm -rf xmr-stak
