ea6e36c759318bc33294175be686112d869a4869
cppdev-123 <45465014+cppdev-123@users.noreply.github.com>
2019-03-20 22:55:39 +0200

    Add files via upload

--- xmrstak/backend/iBackend.hpp
+++ xmrstak/backend/iBackend.hpp
@@ -7,6 +7,9 @@
 #include <climits>
 #include <vector>
 #include <string>
+#include <thread>
+#include <future>
+#include <mutex>
 
 template <typename T, std::size_t N>
 constexpr std::size_t countof(T const (&)[N]) noexcept
@@ -40,11 +43,36 @@ namespace xmrstak
 		std::atomic<uint64_t> iHashCount;
 		std::atomic<uint64_t> iTimestamp;
 		uint32_t iThreadNo;
+		uint64_t thdNo;
 		BackendType backendType = UNKNOWN;
+		std::thread oWorkThd;
+		std::promise<void> numa_promise;
+		std::promise<void> thread_work_promise;
+		// block thread until all NVIDIA GPUs are initialized
+		std::future<void> thread_work_guard;
+		std::promise<void> order_fix;
+		std::mutex thd_aff_set;
+		// volatile becaues the compiler may optimize it in minethd when it sees that is isn't changed there
+		volatile std::atomic<bool> bSuspend;
+		volatile std::atomic<bool> bQuit;
+		volatile std::atomic<bool> pause_idle;
 
-		iBackend() : iHashCount(0), iTimestamp(0)
+		iBackend() : iHashCount(0), iTimestamp(0), bSuspend(false), bQuit(false), pause_idle(false)
 		{
 		}
+
+		void join()
+		{
+			if (oWorkThd.joinable())
+				oWorkThd.join();
+		}
+
+		~iBackend()
+		{
+			bQuit = true;
+			join();
+		}
+
 	};
 
 } // namespace xmrstak
