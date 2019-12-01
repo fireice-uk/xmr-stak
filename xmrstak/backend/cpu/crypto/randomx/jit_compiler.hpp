29682a7d8a71c485873bafac8f872c5af712f5cb
SChernykh <sergey.v.chernykh@gmail.com>
2019-12-01 14:44:53 +0100

    merge randomX changes from xmrig
    merge xmrig 5.1.0 changes

--- xmrstak/backend/cpu/crypto/randomx/jit_compiler.hpp
+++ xmrstak/backend/cpu/crypto/randomx/jit_compiler.hpp
@@ -30,6 +30,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #if defined(_M_X64) || defined(__x86_64__)
 #include "crypto/randomx/jit_compiler_x86.hpp"
+#elif defined(__aarch64__)
+#include "crypto/randomx/jit_compiler_a64.hpp"
 #else
 #include "crypto/randomx/jit_compiler_fallback.hpp"
 #endif
